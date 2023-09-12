ad0230c14fe2a18afdc6b0433dda43de88a63429
Konstantin <nemeshkv@gmail.com>
2023-09-12 22:38:30 +0500

    test1

---
+++ src/ru/nemesh/Main.java
@@ -0,0 +1,19 @@
+package ru.nemesh;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите положительные числа через пробел и нужный оператор +/*- (пример 2 + 3):");
+        int a = 0;
+        int b = 0;
+        int result;
+
+
+
+            result = a + b;
+            System.out.println(result);
+    }
+
+}
